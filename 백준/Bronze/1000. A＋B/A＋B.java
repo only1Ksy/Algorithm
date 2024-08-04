e9e45d9b52ec7000a7d8791ede1a9ade3ef2bc31
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-08-04 22:29:35 +0900

    [Bronze V] Title: A+B, Time: 164 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int A, B;
+		
+		Scanner input = new Scanner(System.in);
+		A = input.nextInt();
+		B = input.nextInt();
+		
+		System.out.println((A+B));
+		
+
+	}
+
+}
