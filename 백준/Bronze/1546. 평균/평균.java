6ce365b81735c3fa0edb512136ea9959a25dfadc
only1Ksy <157667915+only1Ksy@users.noreply.github.com>
2024-09-28 13:08:07 +0900

    [Bronze I] Title: 평균, Time: 284 ms, Memory: 24588 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,38 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        
-        int N, X;
-        double max = 0;
-        double mid = 0;
-        double sum = 0;
-        
-        N = input.nextInt();
-        String temp = input.nextLine();
-        
-        double [] A = new double[N];
-        double [] B = new double[N];        
-        
-        for (int i = 0; i < N; i++) {
-        	A[i] = input.nextDouble();
-        }
-        
-        for (int i = 0; i < N; i++) {
-        	if (A[i] > max)
-        		max = A[i];
-        	else
-        		continue;
-        }
-        
-        for (int i = 0; i < N; i++) {
-        	B[i] = (A[i]/max)*100;
-        	sum += B[i];
-        }
-        
-        System.out.println(sum/N);
-    }
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		input.nextLine();
+		
+		double [] arr1 = new double[n];
+		double [] arr2 = new double[n];
+		double max = 0;
+		
+		for (int i = 0; i < n; i++) {
+			arr1[i] = input.nextDouble();
+			max = Math.max(max, arr1[i]);
+		}
+		
+		double sum = 0;
+		
+		for (int i = 0; i < n; i++) {
+			arr2[i] = arr1[i]/max * 100;
+			sum += arr2[i];
+		}
+		
+		double ave = sum / n;
+		
+		System.out.println(ave);
+	}
 }
